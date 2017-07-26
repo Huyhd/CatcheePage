061dc74e952dbe0f6635eb8d424d1076be768345
huyhd <huyhd.research@gmail.com>
2017-07-26 17:06:24 +0700

    2nd commit2nd commit

--- assets/js/gform.js
+++ assets/js/gform.js
@@ -2,7 +2,7 @@
     $('#signup-form').one('submit',function(){
         var email = encodeURIComponent($('#email').val());
         var ansID = "entry.706696076";
-        var baseURL = 'https://docs.google.com/forms/d/e/1FAIpQLSc05HmQDkHEoT0Dq9udrsNJJ1wvvGXDPn3F8N6qjVw-1KxBQw/formResponse?';
+        var baseURL = 'https://docs.google.com/forms/d/e/1FAIpQLSc05HmQDkHEoT0Dq9udrsNJJ1wvvGXDPn3F8N6qjVw-1KxBQw/formResponse';
         var submitRef = '&submit=7588322602044403867';
         var submitURL = (baseURL + ansID + "=" + email + submitRef);
         console.log(submitURL);
